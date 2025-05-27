a1e4a122e46bfaac5d41bd7d98dc7d55b06154b5
FireStars373 <burausee@gmail.com>
2025-05-28 00:56:58 +0300

    fdsf

--- server/config/swagger.js
+++ server/config/swagger.js
@@ -1,22 +1,22 @@
-const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerJsdoc = require("swagger-jsdoc");
 
 const options = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'KVS Krepsinio Analize API',
-      version: '1.0.0',
-      description: 'API documentation for KVS Krepsinio Analize',
+      title: "KVS Krepsinio Analize API",
+      version: "1.0.0",
+      description: "API documentation for KVS Krepsinio Analize",
     },
     servers: [
       {
-        url: 'http://localhost:5000',
-        description: 'Development server',
+        url: "http://yamabiko.proxy.rlwy.net:34700",
+        description: "Development server",
       },
     ],
   },
-  apis: ['./routes/*.js'], // Path to the API routes
+  apis: ["./routes/*.js"], // Path to the API routes
 };
 
 const specs = swaggerJsdoc(options);
-module.exports = specs; 
+module.exports = specs;
